e20e9aea64f74e6a8abca4df9c4c5f11f68c24f4
YUJI-DESKTOP <yujixr@users.noreply.github.com>
2019-09-29 23:35:29 +0900

    ABC138

--- A.cpp
+++ A.cpp
@@ -1,6 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-  cout << "hello,world" << endl;
+int main()
+{
+    int a;
+    char s[20];
+    scanf("%d", &a);
+    scanf("%s",s);
+    if(a>=3200){
+        printf("%s\n",s);
+    }else{
+        printf("red\n");
+    }
+    return 0;
 }
--- D.cpp
+++ D.cpp
@@ -1,6 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-  cout << "hello,world" << endl;
+int main()
+{
+    int n, q, a[200000], b[200000], p[200000], x[200000], route[200000], result[200000];
+    scanf("%d", &n);
+    scanf("%d", &q);
+    route[0] = 0;
+    result[0] = 0;
+    for (int i = 0; i < n - 1; i++)
+    {
+        scanf("%d", &a[i]);
+        scanf("%d", &b[i]);
+        route[b[i] - 1] = a[i];
+        result[i] = 0;
+    }
+    for (int i = 0; i < q; i++)
+    {
+        scanf("%d", &p[i]);
+        scanf("%d", &x[i]);
+        result[p[i] - 1] += x[i];
+    }
+    for (int i = 0; i < n; i++)
+    {
+        if (route[i])
+        {
+            result[i] += result[route[i] - 1];
+        }
+        printf("%d\n", result[i]);
+    }
+    return 0;
 }
