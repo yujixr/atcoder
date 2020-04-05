b1f97a32ce1a157e349314600802151bd1760886
YUJI <mail@yuji.ne.jp>
2020-04-05 05:24:50 +0100

    Update D.cpp

--- D.cpp
+++ D.cpp
@@ -3,43 +3,23 @@ using namespace std;
 
 int main()
 {
-  bool iss = false;
-  int k, l = 1;
-  vector<int> k3;
+  int k;
   cin >> k;
-
-  for (int re = 9; re < k; re *= 3)
+  queue<int64_t> q({1, 2, 3, 4, 5, 6, 7, 8, 9});
+  for (int i = 0; i < k - 1; i++)
   {
-    re -= l - 1 + max(l - 2, 0);
-    k -= re;
-    iss = k >= re;
-    l++;
+    auto x = q.front();
+    auto mod = x % 10;
+    q.pop();
+    if (mod != 0)
+    {
+      q.push(x * 10 + mod - 1);
+    }
+    q.push(x * 10 + mod);
+    if (mod != 9)
+    {
+      q.push(x * 10 + mod + 1);
+    }
   }
-  k--;
-
-  k3.push_back(k % 3);
-  while (k /= 3)
-  {
-    k3.push_back(k % 3);
-  }
-  reverse(k3.begin(), k3.end());
-  for (auto kk : k3)
-  {
-    cout << kk;
-  }
-  cout << endl;
-
-  int bf = k3[0] + 1, st = 1;
-  if (iss)
-  {
-    bf = k3[0] * 3 + k3[1] + 1;
-    st = 2;
-  }
-  cout << bf;
-  for (int i = st; i < k3.size(); i++)
-  {
-    bf += k3[i] - 1;
-    cout << bf;
-  }
-  cout << endl;
+  cout << q.front() << endl;
 }
