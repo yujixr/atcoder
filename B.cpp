71c227e905089d973cfb926196b58dacb360a022
YUJI <mail@yuji.ne.jp>
2020-06-14 23:00:40 +0900

    revert

--- B.cpp
+++ B.cpp
@@ -3,12 +3,5 @@ using namespace std;
 
 int main()
 {
-  int x, y;
-  cin >> x >> y;
-  if (y % 2 != 0 || y > x * 4 || y < x * 2)
-  {
-    cout << "No" << endl;
-    return 0;
-  }
-  cout << "Yes" << endl;
+  cout << "hello,world" << endl;
 }
