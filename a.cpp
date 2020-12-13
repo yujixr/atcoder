5afba2d51759b7cf58c4501e6fc303eb4a446691
YUJI <mail@yuji.ne.jp>
2020-12-13 13:47:32 +0900

    give up, it's a waste of time.

--- a.cpp
+++ a.cpp
@@ -1,9 +1,40 @@
 #include <bits/stdc++.h>
-#include <atcoder/all>
 using namespace std;
-using namespace atcoder;
 
 int main()
 {
-  cout << "hello,world" << endl;
+  int n, a, ln = 0, rn = 0, step = 0, lst = INT32_MAX, rst = INT32_MIN;
+  string s;
+  cin >> n >> a >> s;
+  s = 'X' + s + 'X';
+  for (int i = 0; i < n + 2; i++)
+  {
+    if (s[i] == '#')
+    {
+      step += abs(a - i) * 2;
+      if (i < a)
+      {
+        ln++;
+        if (lst > i)
+          lst = i;
+      }
+      else
+      {
+        rn++;
+        if (rst < i)
+          rst = i;
+      }
+    }
+  }
+  if (rn > ln)
+  {
+    step += a * 2 * (rn - ln - 1);
+    step -= (rst - a);
+  }
+  else
+  {
+    step += (n + 1 - a) * 2 * (ln - rn);
+    step -= (a - lst);
+  }
+  cout << step << endl;
 }
