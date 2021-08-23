18866352e93d28cbeb2df21c753f7644bd211ef8
YUJI <mail@yuji.ne.jp>
2021-08-23 12:50:23 +0000

    Practice 1

--- a.cpp
+++ a.cpp
@@ -5,5 +5,20 @@ using namespace atcoder;
 
 int main()
 {
-  cout << "hello,world" << endl;
+  int m;
+  cin >> m;
+
+  for (int i = 0; i < m; i++)
+  {
+    string s;
+    cin >> s;
+    replace(s.begin(), s.end(), 'A', 'Z');
+    replace(s.begin(), s.end(), 'T', 'A');
+    replace(s.begin(), s.end(), 'Z', 'T');
+    replace(s.begin(), s.end(), 'C', 'Z');
+    replace(s.begin(), s.end(), 'G', 'C');
+    replace(s.begin(), s.end(), 'Z', 'G');
+    reverse(s.begin(), s.end());
+    cout << s << endl;
+  }
 }
